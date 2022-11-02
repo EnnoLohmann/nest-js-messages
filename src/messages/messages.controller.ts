8cdc715457aeb4447e2635c5bf58118e5b8af020
Enno Lohmann <enno.lohmann@web.de>
2022-11-02 10:35:22 +0100

    initial commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,35 @@
+import {
+  Body,
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+} from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
+
+@Controller('messages')
+export class MessagesController {
+  constructor(private messagesService: MessagesService) {}
+
+  @Get()
+  listMessages() {
+    return this.messagesService.findAll();
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    this.messagesService.create(body.content);
+  }
+
+  @Get(':id')
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (message) {
+      return message;
+    } else {
+      throw new NotFoundException('Message not found');
+    }
+  }
+}
